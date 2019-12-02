4813889baf0234450d787eeb3c0cfb907b0de3b3
Gisle Aune <dev@gisle.me>
2019-12-02 22:10:47 +0100

    add even faster day02 solution, but that was after reading the solutions megathread.

--- src/day02.rs
+++ src/day02.rs
@@ -7,14 +7,17 @@ fn main() {
     let (res_part1, dur_part1) = run_many(10000, || part1(&program));
     let (res_part2, dur_part2) = run_many(10000, || part2(&program));
     let (res_part2_bs, dur_part2_bs) = run_many(10000, || part2_bs(&program));
+    let (res_part2_reduce, dur_part2_reduce) = run_many(10000, || part2_reduce(&program));
 
     print_result("P1", res_part1);
-    print_result("P2 [Pattern Exploit]", res_part2);
-    print_result("P2 [Binary Search]", res_part2_bs);
+    print_result("P2: Pattern Exploit", res_part2);
+    print_result("P2: Binary Search", res_part2_bs);
+    print_result("P2: Reduce [not my idea]", res_part2_reduce);
     print_time("Parse", dur_parse);
     print_time("P1", dur_part1);
-    print_time("P2 [Pattern Exploit]", dur_part2);
-    print_time("P2 [Binary Search]", dur_part2_bs);
+    print_time("P2: Pattern Exploit", dur_part2);
+    print_time("P2: Binary Search", dur_part2_bs);
+    print_time("P2: Reduce [not my idea]", dur_part2_reduce);
 }
 
 fn parse_input(input: &str) -> Vec<u32> {
@@ -101,6 +104,37 @@ fn part2_bs(initial_program: &[u32]) -> u32 {
         program.copy_from_slice(initial_program);
     }
 }
+
+fn part2_reduce(initial_program: &[u32]) -> u32 {
+    let mut program1 = initial_program.to_vec();
+    let mut program2 = initial_program.to_vec();
+    let mut program3 = initial_program.to_vec();
+
+    program1[1] = 25;
+    program1[2] = 11;
+    program2[1] = 26;
+    program2[2] = 11;
+    program3[1] = 25;
+    program3[2] = 12;
+
+    run_intcode(&mut program1);
+    run_intcode(&mut program2);
+    run_intcode(&mut program3);
+
+    let res1 = program1[0];
+    let res2 = program2[0];
+    let res3 = program3[0];
+
+    let x = res2 - res1;
+    let y = res3 - res1;
+    let n = res1 - ((25 * x) + (11 * y));
+
+    let nx = (PART2_TARGET - n) / x;
+    let ny = (PART2_TARGET - n - (nx * x)) / y;
+
+    (nx * 100) + ny
+}
+
 
 fn run_intcode(program: &mut Vec<u32>) {
     let mut position: usize = 0;
