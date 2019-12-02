0842b664351762dff284e486b4d800d5493a4b2b
Gisle Aune <dev@gisle.me>
2019-12-02 15:44:36 +0100

    fix warnings in day02 unit tests.

--- src/day02.rs
+++ src/day02.rs
@@ -107,22 +107,22 @@ mod tests {
     #[test]
     fn test_run_intcode() {
         let mut program = vec![1,0,0,0,99];
-        let mut expected = vec![2,0,0,0,99];
+        let expected = vec![2,0,0,0,99];
         run_intcode(&mut program);
         assert_eq!(program, expected);
 
         let mut program = vec![2,3,0,3,99];
-        let mut expected = vec![2,3,0,6,99];
+        let expected = vec![2,3,0,6,99];
         run_intcode(&mut program);
         assert_eq!(program, expected);
 
         let mut program = vec![2,4,4,5,99,0];
-        let mut expected = vec![2,4,4,5,99,9801];
+        let expected = vec![2,4,4,5,99,9801];
         run_intcode(&mut program);
         assert_eq!(program, expected);
 
         let mut program = vec![1,1,1,4,99,5,6,0,99];
-        let mut expected = vec![30,1,1,4,2,5,6,0,99];
+        let expected = vec![30,1,1,4,2,5,6,0,99];
         run_intcode(&mut program);
         assert_eq!(program, expected);
     }
