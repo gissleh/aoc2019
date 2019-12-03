ec732dda4b90c1a14d1a2d7906dd90d4b22cfc9c
Gisle Aune <dev@gisle.me>
2019-12-03 14:39:33 +0100

    add day03 part2

--- src/day03.rs
+++ src/day03.rs
@@ -3,17 +3,19 @@ use common::aoc::{load_input, run_many, print_time, print_result};
 fn main() {
     let input = load_input("day03");
 
-
     let (wires, dur_parse) = run_many(1000, || parse_input(&input));
     let w1 = &wires[0];
     let w2 = &wires[1];
 
     let (res_part1, dur_part1) = run_many(1000, || w1.closest_intersection(&w2));
+    let (res_part2, dur_part2) = run_many(1000, || w1.shortest_intersection(&w2));
 
     print_result("P1", res_part1.unwrap_or_default());
+    print_result("P2", res_part2.unwrap_or_default());
 
     print_time("Parse", dur_parse);
     print_time("P1", dur_part1);
+    print_time("P2", dur_part2);
 }
 
 fn parse_input(input: &str) -> Vec<Wire> {
@@ -70,6 +72,48 @@ impl Wire {
         winner
     }
 
+    fn shortest_intersection(&self, other: &Wire) -> Option<i32> {
+        let mut winner: Option<i32> = None;
+
+        let mut line_len = 1;
+        let mut prev = &self.points[0];
+        for point in self.points.iter().skip(1) {
+            let line = Line(prev, point);
+
+            let mut other_prev = &other.points[0];
+            let mut other_len = 1;
+            for other_point in other.points.iter().skip(1) {
+                let other_line = Line(other_prev, other_point);
+
+                if let Some(intersect) = line.intersects(&other_line) {
+                    if intersect.x != 0 || intersect.y != 0 {
+                        let line_len2 = line_len + Line(&prev, &intersect).len() - 1;
+                        let other_len2 = other_len + Line(&other_prev, &intersect).len() - 1;
+                        let total_len = line_len2 + other_len2;
+
+                        if let Some(winner_len) = winner {
+                            if total_len < winner_len {
+                                winner = Some(total_len);
+                            }
+                        } else {
+                            winner = Some(total_len);
+                        }
+
+                        break
+                    }
+                }
+
+                other_prev = other_point;
+                other_len += other_line.len();
+            }
+
+            prev = point;
+            line_len += line.len();
+        }
+
+        winner
+    }
+
     fn parse(str: &str) -> Wire {
         let mut wire = Wire {
             points: Vec::with_capacity(str.len() / 3),
@@ -119,7 +163,7 @@ struct Line<'a> (&'a Point, &'a Point);
 impl<'a> Line<'a> {
     fn len(&self) -> i32 {
         let Line(a, b) = *self;
-        
+
         (b.x - a.x).abs() + (b.y - a.y).abs()
     }
 
@@ -162,14 +206,14 @@ mod tests {
     #[test]
     fn test_wire_parse() {
         let actual = Wire::parse("U32,R64,D48,L96,U20");
-        let expected = Wire{
-            points: vec!{
-                Point{x: 0, y: 0},
-                Point{x: 0, y: -32},
-                Point{x: 64, y: -32},
-                Point{x: 64, y: 16},
-                Point{x: -32, y: 16},
-                Point{x: -32, y: -4},
+        let expected = Wire {
+            points: vec! {
+                Point { x: 0, y: 0 },
+                Point { x: 0, y: -32 },
+                Point { x: 64, y: -32 },
+                Point { x: 64, y: 16 },
+                Point { x: -32, y: 16 },
+                Point { x: -32, y: -4 },
             }
         };
 
@@ -188,17 +232,31 @@ mod tests {
     }
 
     #[test]
+    fn test_wire_shortest_intersection() {
+        let w1 = Wire::parse("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+        let w2 = Wire::parse("U62,R66,U55,R34,D71,R55,D58,R83");
+        let w3 = Wire::parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        let w4 = Wire::parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+        let w5 = Wire::parse("R8,U5,L5,D3");
+        let w6 = Wire::parse("U7,R6,D4,L4");
+
+        assert_eq!(w5.shortest_intersection(&w6), Some(30));
+        assert_eq!(w1.shortest_intersection(&w2), Some(610));
+        assert_eq!(w3.shortest_intersection(&w4), Some(410));
+    }
+
+    #[test]
     fn test_line_intersect() {
-        let a = Point{x: 0, y: 0};
-        let b = Point{x: 32, y: 0};
-        let c = Point{x: 16, y: 4};
-        let d = Point{x: 16, y: -4};
-        let e = Point{x: 16, y: -16};
-        let f = Point{x: 16, y: -6};
-        let g = Point{x: 48, y: 0};
-        let h = Point{x: 56, y: 0};
-        let i = Point{x: 48, y: 16};
-        let j = Point{x: 56, y: 16};
+        let a = Point { x: 0, y: 0 };
+        let b = Point { x: 32, y: 0 };
+        let c = Point { x: 16, y: 4 };
+        let d = Point { x: 16, y: -4 };
+        let e = Point { x: 16, y: -16 };
+        let f = Point { x: 16, y: -6 };
+        let g = Point { x: 48, y: 0 };
+        let h = Point { x: 56, y: 0 };
+        let i = Point { x: 48, y: 16 };
+        let j = Point { x: 56, y: 16 };
 
         let ab = Line(&a, &b);
         let cd = Line(&c, &d);
@@ -207,10 +265,24 @@ mod tests {
         let gi = Line(&g, &i);
         let ji = Line(&j, &i);
 
-        assert_eq!(ab.intersects(&cd), Some(Point{x: 16, y: 0}));
+        assert_eq!(ab.intersects(&cd), Some(Point { x: 16, y: 0 }));
         assert_eq!(gh.intersects(&gi), Some(g.clone()));
         assert_eq!(gi.intersects(&ji), Some(i.clone()));
         assert_eq!(ab.intersects(&ef), None);
         assert_eq!(ab.intersects(&gh), None);
+    }
+
+    #[test]
+    fn test_line_len() {
+        let a = Point { x: 0, y: 0 };
+        let b = Point { x: 32, y: 0 };
+        let c = Point { x: 32, y: 4 };
+        let d = Point { x: 36, y: 4 };
+        let e = Point { x: 36, y: -4 };
+
+        assert_eq!(Line(&a, &b).len(), 32);
+        assert_eq!(Line(&b, &c).len(), 4);
+        assert_eq!(Line(&c, &d).len(), 4);
+        assert_eq!(Line(&d, &e).len(), 8);
     }
 }
