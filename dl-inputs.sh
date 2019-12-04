489546899b08ba7e846e7c6879e407b265b46f8f
Gisle Aune <dev@gisle.me>
2019-12-04 23:36:09 +0100

    made dl-inputs.sh more automatic.

--- dl-inputs.sh
+++ dl-inputs.sh
@@ -2,26 +2,20 @@
 
 . ./cookie.env
 
+shopt -s extglob
+
 alias CURL="curl -H 'authority: adventofcode.com' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'sec-fetch-site: same-origin' -H 'sec-fetch-mode: navigate' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: nb,no;q=0.9,nn;q=0.8,en-US;q=0.7,en;q=0.6,de-DE;q=0.5,de;q=0.4' -H 'cookie: session=$AOC_SESSION' --compressed"
 
 mkdir -p input
 
-if [ ! -f "./input/day01.txt" ]; then
-  echo "Getting day01 input..."
-  CURL https://adventofcode.com/2019/day/1/input -o "./input/day01.txt"
-fi
+CURRENT_DAY=$(date +"%e")
 
-if [ ! -f "./input/day02.txt" ]; then
-  echo "Getting day02 input..."
-  CURL https://adventofcode.com/2019/day/2/input -o "./input/day02.txt"
-fi
+for i in $(seq -f "%02g" 1 $CURRENT_DAY)
+do
+  i_nopad=${i##+(0)}
 
-if [ ! -f "./input/day03.txt" ]; then
-  echo "Getting day03 input..."
-  CURL https://adventofcode.com/2019/day/3/input -o "./input/day03.txt"
-fi
-
-if [ ! -f "./input/day04.txt" ]; then
-  echo "Getting day04 input..."
-  CURL https://adventofcode.com/2019/day/4/input -o "./input/day04.txt"
-fi
+  if [ ! -f "./input/day$i.txt" ]; then
+    echo "Getting day$i input..."
+    CURL "https://adventofcode.com/2019/day/$i_nopad/input" -o "./input/day$i.txt"
+  fi
+done
