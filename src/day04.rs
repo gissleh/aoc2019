29faae293353dab7b602de78d12926860a371cdb
Gisle Aune <dev@gisle.me>
2019-12-04 15:27:06 +0100

    add day04 solutions with skipping.

--- src/day04.rs
+++ src/day04.rs
@@ -5,14 +5,20 @@ fn main() {
 
     let ((start, end), dur_parse) = run_many(10000, || parse_input(&input));
     let (res_part1_bf, dur_part1_bf) = run_many(100, || part1_bf(start, end));
+    let (res_part1_skip, dur_part1_skip) = run_many(100, || part1_skip(start, end));
     let (res_part2_bf, dur_part2_bf) = run_many(100, || part2_bf(start, end));
+    let (res_part2_skip, dur_part2_skip) = run_many(100, || part2_skip(start, end));
 
     print_result("P1: Brute Force", res_part1_bf);
+    print_result("P1: Skip", res_part1_skip);
     print_result("P2: Brute Force", res_part2_bf);
+    print_result("P2: Skip", res_part2_skip);
 
     print_time("Parse", dur_parse);
     print_time("P1: Brute Force", dur_part1_bf);
+    print_time("P1: Skip", dur_part1_skip);
     print_time("P2: Brute Force", dur_part2_bf);
+    print_time("P2: Skip", dur_part2_skip);
 }
 
 fn part1_bf(start: u32, end: u32) -> u32 {
@@ -27,6 +33,22 @@ fn part1_bf(start: u32, end: u32) -> u32 {
     count
 }
 
+fn part1_skip(start: u32, end: u32) -> u32 {
+    let mut count = 0;
+
+    let mut i = start;
+    while i <= end {
+        let (valid, step) = password_valid_skip(i);
+        if valid {
+            count += 1;
+        }
+
+        i += step;
+    }
+
+    count
+}
+
 fn part2_bf(start: u32, end: u32) -> u32 {
     let mut count = 0;
 
@@ -34,6 +56,23 @@ fn part2_bf(start: u32, end: u32) -> u32 {
         if password_valid_p2(i) {
             count += 1;
         }
+    }
+
+    count
+}
+
+
+fn part2_skip(start: u32, end: u32) -> u32 {
+    let mut count = 0;
+
+    let mut i = start;
+    while i <= end {
+        let (valid, step) = password_valid_p2_skip(i);
+        if valid {
+            count += 1;
+        }
+
+        i += step;
     }
 
     count
@@ -71,6 +110,36 @@ fn password_valid(n: u32) -> bool {
     has_double
 }
 
+fn password_valid_skip(n: u32) -> (bool, u32) {
+    let mut has_double = false;
+    let mut prev_digit = n % 10;
+    let mut n = n / 10;
+    let mut skip = 1;
+
+    loop {
+        let curr_digit = n % 10;
+        if curr_digit > prev_digit {
+            if skip > 1 {
+                skip = skip / 10;
+            }
+
+            return (false, skip);
+        } else if curr_digit == prev_digit {
+            has_double = true;
+        }
+
+        prev_digit = curr_digit;
+        n /= 10;
+        skip *= 10;
+
+        if n == 0 {
+            break;
+        }
+    }
+
+    (has_double, 1)
+}
+
 fn password_valid_p2(n: u32) -> bool {
     let mut has_double = 10;
     let mut bad_double = 10;
@@ -104,6 +173,47 @@ fn password_valid_p2(n: u32) -> bool {
 
     has_double != 10 && has_double != bad_double
 }
+
+fn password_valid_p2_skip(n: u32) -> (bool, u32) {
+    let mut has_double = 10;
+    let mut bad_double = 10;
+    let mut prev_double = false;
+    let mut prev_digit = n % 10;
+    let mut n = n / 10;
+    let mut skip = 1;
+
+    loop {
+        let curr_digit = n % 10;
+        if curr_digit > prev_digit {
+            if skip > 1 {
+                skip = skip / 10;
+            }
+
+            return (false, skip);
+        } else if curr_digit == prev_digit {
+            if prev_double && curr_digit == has_double {
+                bad_double = curr_digit;
+            } else if has_double == 10 || has_double == bad_double {
+                has_double = curr_digit;
+            }
+
+            prev_double = true;
+        } else {
+            prev_double = false;
+        }
+
+        prev_digit = curr_digit;
+        n /= 10;
+        skip *= 10;
+
+        if n == 0 {
+            break;
+        }
+    }
+
+    (has_double != 10 && has_double != bad_double, 1)
+}
+
 
 #[cfg(test)]
 mod tests {
